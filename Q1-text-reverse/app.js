1ab3193fd2a0dc077a519f0fb3a6b5e92460e4ee
RediChen <a0929052958@gmail.com>
2024-05-13 23:42:20 +0800

    完成Q1程式邏輯 但是樣式尚未完成

--- Q1-text-reverse/app.js
+++ Q1-text-reverse/app.js
@@ -1,9 +1,15 @@
+const inputString = document.querySelector("#reverse-input");
 const result = document.querySelector('#result');
 
 const exeReverse = (input) => {
     const charArr = input.split("");
     result.innerHTML = charArr.reverse().join("");
+    result.classList.add('active');
 };
 
-const inputString = document.querySelector("#reverse-input");
-inputString.addEventListener("keyup", e => exeReverse(e.value));
+inputString.addEventListener("keydown", () =>{
+    result.classList.remove('active');
+});
+inputString.addEventListener("keyup", function (){
+    exeReverse(this.value);
+});
