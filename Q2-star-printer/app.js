43e20a497daff47317b6076b51fb64bedd8be688
RediChen <a0929052958@gmail.com>
2024-05-14 17:30:46 +0800

    完成 Q2

---
+++ Q2-star-printer/app.js
@@ -0,0 +1,37 @@
+const numberInput = document.querySelector('#star-number');
+const btn = document.querySelector('#btn');
+const monitor = document.querySelector('#monitor');
+const COL_BOUND = 10;
+const SIGN = '☆';
+
+const drawStars = () => {
+    const target = parseInt(numberInput.value);
+    const rowNum = Math.floor(target / COL_BOUND);
+    const restNum = target % COL_BOUND;
+    let star_row = '';
+    for (let i = 0; i < COL_BOUND; i++) {
+        star_row += SIGN;
+    }
+    const lineIndex = n => `<span class="line-index">${n}</span>&nbsp;`;
+    let output = '';
+
+    //滿排的部份
+    if (rowNum > 0) {
+        for (let i = 0; i < rowNum; i++) {
+            output += '<p>' + lineIndex(i + 1) + star_row + '</p>';
+        }
+    }
+
+    //零星的部份
+    let star_rest = '';
+    for (let i = 0; i < restNum; i++) {
+        star_rest += SIGN;
+    }
+    output += '<p>' + lineIndex(rowNum + 1) + star_rest + '</p>';
+
+    monitor.innerHTML = output;
+};
+btn.addEventListener('click', drawStars);
+numberInput.addEventListener('keyup', e => {
+    if (e.keyCode === 13) drawStars();
+})
