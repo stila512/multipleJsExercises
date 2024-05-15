4d0addb020ff6f9534c179f977c99ec7a4e8f889
RediChen <a0929052958@gmail.com>
2024-05-15 17:25:38 +0800

    完成 Q4

---
+++ Q4-array-filter/app.js
@@ -0,0 +1,47 @@
+const numArr = [1, 35, 64, 9, -5, 37, 11, 27, 90, -41, -26, 50, -11];
+const box = document.querySelector('#number-box');
+const input = document.querySelector('#input');
+const btn = document.querySelector('#btn');
+
+//* 初始畫面
+let boxStr = '';
+for (num of numArr) {
+    boxStr += '<div class="number-plate">' + num + '</div>';
+}
+box.innerHTML = boxStr;
+const plateArr = document.querySelectorAll('.number-plate');
+
+
+//* 更新畫面
+const deactivateAll = () => {
+    for (plate of plateArr) {
+        plate.classList.remove('active');
+    }
+}
+
+let upperBound = 0;
+const updateBox = () => {
+    deactivateAll();
+    upperBound = parseInt(input.value);
+    const selectedNumArr = numArr.filter(num => num < upperBound);
+
+    for (num of numArr) {
+        if (selectedNumArr.includes(num)) {
+            plateArr[numArr.indexOf(num)].classList.add('active');
+        }
+    }
+}
+
+//* 綁定事件
+btn.addEventListener('click', updateBox);
+
+input.addEventListener('keydown', e => {
+    if (e.keyCode === 13)  updateBox();
+})
+
+
+
+
+
+
+
